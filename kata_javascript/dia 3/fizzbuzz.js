b8ca7ddbe30d47a1643261d9c42c9b7f6210b420
itzelgiron22 <itzegiron22@gmail.com>
2022-08-19 22:04:00 -0500

    Create fizzbuzz.js

---
+++ kata_javascript/dia 3/fizzbuzz.js
@@ -0,0 +1,12 @@
+for ( let index=1; index<=100; index++)
+{
+    let resultado = index %5 == 0}
+
+{ let resultado = index %3 == 0} 
+
+if (resultado==%3)
+    console.log ("fizz") 
+if ( resultado ==%5) 
+    console.log("buzz")
+else (resultado == %3 && %5)
+console.log("fizz/buzz") 
